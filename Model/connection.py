396d040c023ddcf5538776e774474a983e4d1b3c
Jesica <jesicapaola51@gmail.com>
2019-06-30 20:52:51 -0300

    cambios en la config de la base de datos

--- Model/connection.py
+++ Model/connection.py
@@ -2,10 +2,10 @@ import mysql.connector
 from mysql.connector import Error
 
 mydb = mysql.connector.connect(
-  host='127.0.0.1',
-  user='kary',
-  passwd='holly',
-  port='33061',
+  host='localhost',
+  user='jessica',
+  passwd='morena2019',
+  port='3306',
   auth_plugin='mysql_native_password',
   database='clinica'
 )
