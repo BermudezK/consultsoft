1eb7f447d5ce2038eb9d89f640876154615c4479
Luciano Suarez <luciano-ipp@hotmail.com>
2019-07-03 18:54:21 -0300

    Login funcionando sin ventana por rol

--- Controller/ventana_login.py
+++ Controller/ventana_login.py
@@ -1,8 +1,10 @@
-import sys, re
+import sys
+import re
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
 from PyQt5 import uic
 from Model.administrador import Administrador
 from Model.personal_query import logIn
+
 
 class VentanaLogin(QDialog):
   def __init__(self):
@@ -31,7 +33,7 @@ class VentanaLogin(QDialog):
     self.Password.setMaxLength(8)
     Password = self.Password.text()
     validar = re.match("^[A-Z\sáéíóúàèìùäëïöüñ\0-9]{2,8}$", Password, re.I)
-    
+
     if Password == "":
       return False
     elif not validar:
@@ -42,16 +44,20 @@ class VentanaLogin(QDialog):
   def submitAcceder(self):
     if self.validar_Password() and self.validar_User():
       print('Ok')
+      QMessageBox.warning(
+          self, "Login Correcto!!", "Bienvenido al sistema.", QMessageBox.Discard)
 
     else:
       print('Mal')
+      QMessageBox.warning(
+          self, "Error", "Usuario no existe o contraseña incorrecta, Intente nuevamente", QMessageBox.Discard)
 
     styleValid = "border: 1px solid green; background-color: transparent;"
     styleInvalid = "border: 1px solid red; background-color: transparent;"
 
     if self.validar_Password():
       self.Password.setStyleSheet(styleValid)
-    else: 
+    else:
       self.Password.setStyleSheet(styleInvalid)
 
     if self.validar_User():
@@ -63,28 +69,28 @@ class VentanaLogin(QDialog):
 
     if usuario:
       usuario_id, username, personal_dni, rol_id, nombre_rol, nombre, apellido = usuario
-      print(usuario_id, username, personal_dni, rol_id, nombre_rol, nombre, apellido)
-      # QMessageBox.information(self, f"Bienvenido {apellido}, {nombre}. Rol {nombre_rol}", QMessageBox.Discard)
-    else:
-      QMessageBox.warning(self,"Ingreso", "¡Disfrute de nuestro sistema nae!!")
-  
+      print(usuario_id, username, personal_dni,
+            rol_id, nombre_rol, nombre, apellido)
+      QMessageBox.information(
+          self, f"Bienvenido {apellido}, {nombre}. Rol {nombre_rol}", QMessageBox.Discard)
+    #else:
+      #QMessageBox.warning(self,"Ingreso", "Error al ingresar")
+
   # usuario
 
   # SELECT * FROM usuario AS u
-  #   WHERE 
+  #   WHERE
   #     u.userName = userName AND
   #     u.password = password AND
   #     (SELECT * FROM personal AS p
-  #       WHERE 
+  #       WHERE
   #         p.usuario_DNI = u.usuario_ID);
 
-  
-  
   # personal
   # rol
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     dialogo = VentanaLogin()
     dialogo.show()
