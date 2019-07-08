6e9c6cf535497f7b61b15d80f0a172c056ec055a
Pablo Barrios <exepablo96@gmail.com>
2019-07-08 16:39:55 -0300

    modifique la interfaz

--- Model/connection.py
+++ Model/connection.py
@@ -2,11 +2,9 @@ import mysql.connector
 from mysql.connector import Error
 
 mydb = mysql.connector.connect(  
-  host='127.0.0.1',
-  user='kary',
-  passwd='holly',
-  port='33061',
-  auth_plugin='mysql_native_password',
+  host='localhost',
+  user='root',
+  passwd='1234',
   database='clinica'
 )
 
