61145f663adf77b29db8608354b1a5322bd3ec7f
Karina Bermúdez <karysol_92@hotmail.com>
2019-07-09 15:51:50 -0300

    modulo de conexion agregado

---
+++ Model/connection.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import Error
+
+mydb = mysql.connector.connect(  
+  host='127.0.0.1',
+  user='kary',
+  passwd='holly',
+  port='33061',
+  auth_plugin='mysql_native_password',
+  database='clinica'
+)
+
+def select_personal(rol):
+    try:
+        if mydb.is_connected():
+            cursor = mydb.cursor()
+            consulta = 'SELECT personal_DNI,nombre, apellido, telefono FROM personal WHERE rol_id = {rol};'
+            cursor.execute(consulta)
+            resultado = cursor.fetchall()
+            return resultado
+    except Error as e :
+        print ("Error while connecting to MySQL", e)
+    finally:
+        #closing database connection.
+        if(mydb.is_connected()):
+            cursor.close()
