11171ffb21fdeb2cd3833c663115f580f0898a02
Raizu <rizzoli548luis@outlook.com>
2019-07-09 15:14:27 -0300

    conexion

--- Model/connection.py
+++ Model/connection.py
@@ -3,10 +3,8 @@ from mysql.connector import Error
 
 mydb = mysql.connector.connect(  
   host='127.0.0.1',
-  user='kary',
-  passwd='holly',
-  port='33061',
-  auth_plugin='mysql_native_password',
+  user='root',
+  passwd='09021998',
   database='clinica'
 )
 
